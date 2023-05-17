a84f8c11bf61d19d42ba524709c3b09a6c9fda11
Muser0030 <117852918+Muser0030@users.noreply.github.com>
2023-05-17 11:21:29 +0300

    Update 11-best_score.sql

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -2,7 +2,7 @@
 -- in the table `second_table` of the
 -- database `hbtn_0c_0` in MySQL Server.
 
-SELECT 'score', 'name'
-FROM 'second_table'
-WHERE 'score' >= 10
-ORDER BY 'score' DESC;
+SELECT score, name
+FROM second_table
+WHERE `score` >= 10
+ORDER BY score DESC;
