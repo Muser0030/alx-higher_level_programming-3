7d1f61941c703fa9fb0ae30026bfc58bcdf7330f
Muser0030 <117852918+Muser0030@users.noreply.github.com>
2023-05-17 10:45:12 +0300

    Update 12-no_cheating.sql

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,3 +1,6 @@
-UPDATE 'second_table'
-SET 'score' = 10
-WHERE 'name' = "Bob";
+-- Updates the score of a record in the
+-- table `second_table` in MySQL Server.
+
+UPDATE second_table
+SET `score` = '10'
+WHERE `name` = 'Bob';
