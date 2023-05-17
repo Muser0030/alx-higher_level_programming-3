9af418e4359ff26aee52945995ea3d161176fe83
Muser0030 <117852918+Muser0030@users.noreply.github.com>
2023-05-17 10:49:17 +0300

    Update 16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,7 @@
-SELECT 'score', 'name'
-FROM 'second_table'
-WHERE 'name' is ""
-ORDER BY 'score' DESC;
+-- Lists all records of the table `second_table`
+-- of the database `hbtn_0c_0` in MySQL Server.
+
+SELECT score, name
+FROM second_table
+WHERE `name` IS NOT NULL
+ORDER BY score DESC;
